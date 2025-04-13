18dcec289cf558773aa3226a7d90c8ab2f8ba5f7
Manish kumar vishwakarma <152419999+Manish3737@users.noreply.github.com>
2025-04-13 15:03:42 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 print("AI-Adaptive-Aero-F1 System Initialized")
+from sensors import get_sensor_data
+from ai_engine import decide_adjustments
+from controller import apply_adjustments
+
+def run_system():
+    print("Starting AI-Adaptive-Aero-F1 System...")
+    sensor_data = get_sensor_data()
+    decisions = decide_adjustments(sensor_data)
+    apply_adjustments(decisions)
+
+if __name__ == "__main__":
+    run_system()
